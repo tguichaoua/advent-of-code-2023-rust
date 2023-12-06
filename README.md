9ef50865827fcba19adb6c48b940ad00117bb3dd
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2023-12-06 11:21:50 +0100

    🎄Day 06

--- README.md
+++ README.md
@@ -23,11 +23,12 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `36.8µs` | `169.6µs` |
-| [Day 2](./src/bin/02.rs) | `54.0µs` | `53.0µs` |
-| [Day 3](./src/bin/03.rs) | `50.5µs` | `137.3µs` |
-| [Day 4](./src/bin/04.rs) | `106.2µs` | `105.1µs` |
-| [Day 5](./src/bin/05.rs) | `15.3µs` | `28.5µs` |
+| [Day 1](./src/bin/01.rs) | `37.0µs` | `169.6µs` |
+| [Day 2](./src/bin/02.rs) | `52.4µs` | `53.0µs` |
+| [Day 3](./src/bin/03.rs) | `53.5µs` | `137.3µs` |
+| [Day 4](./src/bin/04.rs) | `104.5µs` | `106.3µs` |
+| [Day 5](./src/bin/05.rs) | `15.0µs` | `28.0µs` |
+| [Day 6](./src/bin/06.rs) | `185.0ns` | `338.0ns` |
 
 **Total: 0.76ms**
 <!--- benchmarking table --->
---
+++ src/bin/06.rs
@@ -0,0 +1,138 @@
+use itertools::Itertools;
+
+advent_of_code::solution!(6);
+
+/* -------------------------------------------------------------------------- */
+
+fn calculate_nb_solution_for_race(time: u64, distance: u64) -> u64 {
+    // Let `d` the distance the boat travel
+    // Let `D` the distance to beat
+    // Let `t` the time spent to press the button
+    // Let `T` the total time for the race
+    // Let `v` the speed of the boat
+
+    // The speed of the boat increase by 1 mm/ms for each ms the button is pressed:
+    // v = t . 1
+
+    // We want
+    //            d > D
+    //      v.(T-t) > D
+    //      t.(T-t) > D
+    //      tT - t² > D
+    // -t² + tT - D > 0
+
+    // We have a polynomial aX² + bX + c
+    // with a = -1
+    //      b = T
+    //      c = -D
+
+    // Since a < 0, the polynomial is strictly positive for t_1 < t < t_2
+    // with t_1 and t_2 the roots of the polynomial.
+
+    // Let's calculate the roots of -t² + Tt - D
+
+    // delta = b² - 4ac
+    //       = T² - 4.(-1).(-D)
+    //       = T² - 4.D
+    let Some(delta) = (time * time).checked_sub(4 * distance) else {
+        // no real solution
+        return 0;
+    };
+
+    let sqrt_delta = f64::sqrt(delta as f64);
+
+    // t_1,2 = ( -b ± sqrt(delta) ) / ( 2.a )
+    //       = ( -T ± sqrt(delta) ) / ( 2.(-1) )
+    //       = (  T ± sqrt(delta) ) / ( 2 )
+    let t_1 = (time as f64 - sqrt_delta) / 2.0;
+    let t_2 = (time as f64 + sqrt_delta) / 2.0;
+
+    // Since we can only press the button for an integer amount of time
+    // we need to round t_1 and t_2.
+
+    let t_1 = if t_1 < 0.0 {
+        // time cannot be negative
+        0
+    } else {
+        // We want the integer greater than t_1 but not t_1 itself (if it's an integer),
+        // because the inequality above is strict.
+        (t_1 + 1.0).floor() as u64
+    };
+
+    let t_2 = {
+        // Same as of t_1, we want the integer lower than t_2 but not t_2 itself.
+        (t_2 - 1.0).ceil() as u64
+    };
+
+    // The number of solution is the number of integer value between t_1 and t_2.
+    t_2 - t_1 + 1
+}
+
+/* -------------------------------------------------------------------------- */
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (times, distances) = input.split_once('\n').unwrap();
+
+    let times = times
+        .strip_prefix("Time:")
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap());
+
+    let distances = distances
+        .strip_prefix("Distance:")
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap());
+
+    let result = times
+        .zip_eq(distances)
+        .map(|(time, distance)| calculate_nb_solution_for_race(time, distance))
+        .product1()
+        .unwrap();
+
+    Some(result)
+}
+
+/* -------------------------------------------------------------------------- */
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (times, distances) = input.split_once('\n').unwrap();
+
+    let time = times
+        .strip_prefix("Time:")
+        .unwrap()
+        .replace(|c: char| c.is_whitespace(), "")
+        .parse::<u64>()
+        .unwrap();
+
+    let distance = distances
+        .strip_prefix("Distance:")
+        .unwrap()
+        .replace(|c: char| c.is_whitespace(), "")
+        .parse::<u64>()
+        .unwrap();
+
+    let result = calculate_nb_solution_for_race(time, distance);
+
+    Some(result)
+}
+
+/* -------------------------------------------------------------------------- */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(288));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(71503));
+    }
+}
